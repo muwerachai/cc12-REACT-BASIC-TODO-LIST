11d2b49868d58d6788c857e698fa67a691d26cee
werachai <>
2024-04-23 12:28:36 +0700

    add search status edit bar seardh text

--- src/student/App.js
+++ src/student/App.js
@@ -1,5 +1,17 @@
+import EditBar from "./components/Editbar";
+import SearchBox from "./components/SearchBox";
+import SearchStatus from "./components/SearchStatus";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="container mt-5 mb-3 max-w-xs">
+      <EditBar />
+      <div className="d-flex gap-3 my-4">
+        <SearchBox />
+        <SearchStatus />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/student/components/SearchBox.js
@@ -0,0 +1,12 @@
+function SearchBox() {
+  return (
+    <div className="input-group">
+      <input type="text" className="form-control " />
+      <button className="btn btn-secondary">
+        <i className="fa-solid fa-xmark" />
+      </button>
+    </div>
+  );
+}
+
+export default SearchBox;
